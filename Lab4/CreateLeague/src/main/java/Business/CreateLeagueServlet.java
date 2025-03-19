d81261df4befe098bed2baf26a343ddf5bf09e3f
AananthK <aananthk15@gmail.com>
2025-03-18 21:38:48 -0400

    Modify CreateLeague microservice to output xml and delete unnecessary packages

--- Lab4/CreateLeague/src/main/java/Business/CreateLeagueServlet.java
+++ Lab4/CreateLeague/src/main/java/Business/CreateLeagueServlet.java
@@ -6,20 +6,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import Endpoint.LeagueRequest;
 
 @WebServlet("/createLeagueServlet")
 public class CreateLeagueServlet extends HttpServlet {
-    private CreateLeagueService leagueService = new CreateLeagueService();
+    private CreateLeagueService leagueService = new CreateLeagueService(); //object that creates leagues
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        // get user input
         String leagueName = request.getParameter("leagueName");
         int managerID = Integer.parseInt(request.getParameter("managerID"));
 
-        boolean success = leagueService.createLeague(leagueName, managerID);
+        // formatting for responses after league creation attempt
+        response.setContentType("text/xml");
+        response.setCharacterEncoding("UTF-8");
+        
+        boolean success = leagueService.createLeague(leagueName, managerID); // league is created: returns true is successful
         if (success) {
-            response.getWriter().write("<html><body><h1>League created successfully</h1></body></html>");
+            
+            LeagueRequest Lreq = new LeagueRequest(); // class to get League attributes as XML
+            Lreq.setName(leagueName);
+            Lreq.setManagerID(managerID);
+            
+            response.getWriter().write("<response><message>League created successfully</message><league><name>" + Lreq.getName() + "</name><managerID>" + Lreq.getManagerID() + "</managerID></league></response>");
         } else {
-            response.getWriter().write("<html><body><h1>Error creating league</h1></body></html>");
+            response.getWriter().write("<response><message>Error creating league</message></response>");
         }
     }
 }
